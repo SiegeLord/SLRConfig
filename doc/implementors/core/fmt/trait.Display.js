2db4b800f3d1d357e1458f9b3d90da918be2759f
Travis <travis@travis-ci.org>
2015-08-31 03:14:44 +0000

    Update docs from Travis build 56

--- doc/implementors/core/fmt/trait.Display.js
+++ doc/implementors/core/fmt/trait.Display.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['slr_config'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='slr_config/struct.ConfigElement.html' title='slr_config::ConfigElement'>ConfigElement</a>",];
+implementors['slr_config'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='slr_config/struct.ConfigElement.html' title='slr_config::ConfigElement'>ConfigElement</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);

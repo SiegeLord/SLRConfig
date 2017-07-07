f41f1844dcf81b83622343b8b10858d08635460b
Travis <travis@travis-ci.org>
2017-07-07 05:08:15 +0000

    Update docs from Travis build 448

--- doc/implementors/alloc/allocator/trait.Alloc.js
+++ doc/implementors/alloc/allocator/trait.Alloc.js
@@ -1,7 +1,7 @@
 (function() {var implementors = {};
-implementors["serde"] = [];
-implementors["slr_config"] = [];
-implementors["syn"] = [];
+implementors["serde"] = ["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/allocator/trait.Alloc.html\" title=\"trait alloc::allocator::Alloc\">Alloc</a> for &amp;'a System","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/allocator/trait.Alloc.html\" title=\"trait alloc::allocator::Alloc\">Alloc</a> for System",];
+implementors["slr_config"] = ["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/allocator/trait.Alloc.html\" title=\"trait alloc::allocator::Alloc\">Alloc</a> for &amp;'a System","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/allocator/trait.Alloc.html\" title=\"trait alloc::allocator::Alloc\">Alloc</a> for System",];
+implementors["syn"] = ["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/allocator/trait.Alloc.html\" title=\"trait alloc::allocator::Alloc\">Alloc</a> for &amp;'a System","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/allocator/trait.Alloc.html\" title=\"trait alloc::allocator::Alloc\">Alloc</a> for System",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
---
+++ doc/implementors/compiler_builtins/float/trait.Float.js
@@ -0,0 +1,12 @@
+(function() {var implementors = {};
+implementors["serde"] = [];
+implementors["slr_config"] = [];
+implementors["syn"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()

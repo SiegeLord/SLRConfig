2d20e75e953cfd870941ff6f8d04284a646b6833
Travis <travis@travis-ci.org>
2020-10-09 10:07:46 +0000

    Update docs from Travis build 1650

--- doc/implementors/serde/de/trait.Error.js
+++ doc/implementors/serde/de/trait.Error.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {};
 implementors["serde"] = [];
-implementors["slr_parser"] = [{"text":"impl <a class=\"trait\" href=\"serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"struct\" href=\"slr_parser/struct.Error.html\" title=\"struct slr_parser::Error\">Error</a>","synthetic":false,"types":["slr_parser::lexer::Error"]}];
+implementors["slr_parser"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

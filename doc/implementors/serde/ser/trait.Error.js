4594e10aaa039e43906f249a709e94667058efaa
Travis <travis@travis-ci.org>
2021-03-26 06:05:49 +0000

    Update docs from Travis build 1816

--- doc/implementors/serde/ser/trait.Error.js
+++ doc/implementors/serde/ser/trait.Error.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {};
 implementors["serde"] = [];
-implementors["slr_parser"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
+implementors["slr_parser"] = [{"text":"impl <a class=\"trait\" href=\"serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"struct\" href=\"slr_parser/struct.Error.html\" title=\"struct slr_parser::Error\">Error</a>","synthetic":false,"types":["slr_parser::lexer::Error"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- doc/implementors/std/error/trait.Error.js
+++ doc/implementors/std/error/trait.Error.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {};
-implementors["proc_macro2"] = [{"text":"impl Error for LexError","synthetic":false,"types":[]}];
-implementors["serde"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
-implementors["slr_parser"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
-implementors["syn"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
+implementors["proc_macro2"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"proc_macro2/struct.LexError.html\" title=\"struct proc_macro2::LexError\">LexError</a>","synthetic":false,"types":["proc_macro2::LexError"]}];
+implementors["serde"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"serde/de/value/struct.Error.html\" title=\"struct serde::de::value::Error\">Error</a>","synthetic":false,"types":["serde::de::value::Error"]}];
+implementors["slr_parser"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"slr_parser/struct.Error.html\" title=\"struct slr_parser::Error\">Error</a>","synthetic":false,"types":["slr_parser::lexer::Error"]}];
+implementors["syn"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"syn/parse/struct.Error.html\" title=\"struct syn::parse::Error\">Error</a>","synthetic":false,"types":["syn::error::Error"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
